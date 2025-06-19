2e4191b91acc9153f0bec74d9a48dc476597c7ae
m7md3bbas <mhmdabass330@gmail.com>
2025-06-19 20:17:33 +0200

    finishedtak

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { TextInput, Text, TouchableOpacity, View } from 'react-native';
+import { styles } from './style';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ ...styles.container, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 20, color: "white", marginBottom: 20 }}>Todo App</Text>
+      <TextInput style={styles.input}> Enter Title</TextInput>
+      <TextInput style={styles.input}> Enter Description</TextInput>
+      <TouchableOpacity style={styles.submitBtn}><Text style={{ ...styles.text, fontWeight: "bold" }}>Submit</Text></TouchableOpacity>
+      <View style={styles.dividerLine}></View>
+      <View style={styles.filterContainer}>
+        <TouchableOpacity style={styles.filterBtn}><Text style={styles.filterText}>All</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.filterBtn}><Text style={styles.filterText}>Active</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.filterBtn}><Text style={styles.filterText}>Completed</Text></TouchableOpacity>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
